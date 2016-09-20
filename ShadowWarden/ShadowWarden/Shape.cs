02a75ae009a2683e56d5405072f38b3c1d820313
timmetry <doctort144@gmail.com>
2016-09-20 16:52:34 -0500

    Migrating project to new repository under username "timmetry".

---
+++ ShadowWarden/ShadowWarden/Shape.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ShadowWarden
+{
+    public abstract class Shape
+    {
+        public abstract Rect4 TestCollision(Point2 b, Vect4 mov);
+        public abstract Rect4 TestCollision(Point3 b, Vect4 mov);
+        public abstract Rect4 TestCollision(Point4 b, Vect4 mov);
+
+        public abstract Rect4 TestCollision(Rect2 b, Vect4 mov);
+        public abstract Rect4 TestCollision(Rect3 b, Vect4 mov);
+        public abstract Rect4 TestCollision(Rect4 b, Vect4 mov);
+    }
+
+    public class Point2 : Shape
+    {
+        public Vect2 pos;
+
+        public Point2() { }
+        public Point2(Vect2 pos)
+        {
+            this.pos = pos;
+        }
+        public Point2(double x, double y)
+        {
+            this.pos = new Vect2(x, y);
+        }
+
+        public override Rect4 TestCollision(Point2 b, Vect4 mov)
+        { return Rect4.Zero; }
+        public override Rect4 TestCollision(Point3 b, Vect4 mov)
+        { return new HitBox(pos.x + mov.x == b.pos.x && pos.y + mov.y == b.pos.y); }
+        public override Rect4 TestCollision(Point4 b, Vect4 mov)
+        { return new HitBox(pos.x + mov.x == b.pos.x && pos.y + mov.y == b.pos.y); }
+
+        public override Rect4 TestCollision(Rect2 r, Vect4 mov)
+        {
+            if (pos.x + mov.x >= r.pos.x && pos.x + mov.x <= r.size.x &&
+                pos.y + mov.y >= r.pos.y && pos.y + mov.y <= r.size.y)
+            {
+            }
+        }
+    }
+
+    public class Point3 : Shape
+    {
+        public Vect3 pos;
+
+        public Point3() { }
+        public Point3(Vect3 pos)
+        {
+            this.pos = pos;
+        }
+        public Point3(double x, double y, double u)
+        {
+            this.pos = new Vect3(x, y, u);
+        }
+    }
+
+    public class Point4 : Shape
+    {
+        public Vect4 pos;
+
+        public Point4() { }
+        public Point4(Vect4 pos)
+        {
+            this.pos = pos;
+        }
+        public Point4(double x, double y, double u, double v)
+        {
+            this.pos = new Vect4(x, y, u, v);
+        }
+    }
+
+    public class Rect2 : Shape
+    {
+        public Vect2 pos;
+        public Vect2 size;
+
+        public double Left { get { return pos.x; } set { pos.x = value; } }
+        public double Top { get { return pos.y; } set { pos.y = value; } }
+        public double Width { get { return size.x; } }
+        public double Height { get { return size.y; } }
+        public double Right { get { return pos.x + size.x; } set { pos.x = value - size.x; } }
+        public double Bottom { get { return pos.y + size.y; } set { pos.y = value - size.y; } }
+
+        public Vect2 Center { get { return pos + (size / 2); } set { pos = value - (size / 2); } }
+
+        public Rect2() { }
+        public Rect2(Vect2 pos, Vect2 size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+        public Rect2(double x, double y, double width, double height)
+        {
+            this.pos = new Vect2(x, y);
+            this.size = new Vect2(width, height);
+        }
+    }
+
+    public class Rect3 : Shape
+    {
+        public Vect3 pos;
+        public Vect3 size;
+
+        public double Left { get { return pos.x; } set { pos.x = value; } }
+        public double Top { get { return pos.y; } set { pos.y = value; } }
+        public double Width { get { return size.x; } }
+        public double Height { get { return size.y; } }
+        public double Right { get { return pos.x + size.x; } set { pos.x = value - size.x; } }
+        public double Bottom { get { return pos.y + size.y; } set { pos.y = value - size.y; } }
+
+        public Vect3 Center { get { return pos + (size / 2); } set { pos = value - (size / 2); } }
+
+        public Rect3() { }
+        public Rect3(Vect3 pos, Vect3 size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+        public Rect3(double x, double y, double u, double width, double height, double uWidth)
+        {
+            this.pos = new Vect3(x, y, u);
+            this.size = new Vect3(width, height, uWidth);
+        }
+    }
+
+    public class Rect4 : Shape
+    {
+        public Vect4 pos;
+        public Vect4 size;
+
+        public double Left { get { return pos.x; } set { pos.x = value; } }
+        public double Top { get { return pos.y; } set { pos.y = value; } }
+        public double Width { get { return size.x; } }
+        public double Height { get { return size.y; } }
+        public double Right { get { return pos.x + size.x; } set { pos.x = value - size.x; } }
+        public double Bottom { get { return pos.y + size.y; } set { pos.y = value - size.y; } }
+
+        public Vect4 Pos2 { get { return pos + size; } set { pos = value - size; } }
+        public Vect4 Center { get { return pos + (size / 2); } set { pos = value - (size / 2); } }
+
+        public Rect4() { }
+        public Rect4(Vect4 pos, Vect4 size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+        public Rect4(double x, double y, double u, double v, double width, double height, double uWidth, double vHeight)
+        {
+            this.pos = new Vect4(x, y, u, v);
+            this.size = new Vect4(width, height, uWidth, vHeight);
+        }
+    }
+}
---
+++ ShadowWarden/ShadowWarden/Vect.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ShadowWarden
+{
+    public class Vect2
+    {
+        public double x;
+        public double y;
+
+        /// <summary>
+        /// Magnitude of a vector: its length calculated by the Pythagorean Theorem
+        /// </summary>
+        public double M { get { return Math.Sqrt(x * x + y * y); } }
+
+        public Vect2(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vect2 Zero
+        { get { return new Vect2(0, 0); } }
+        public static Vect2 Unit
+        { get { return new Vect2(1, 1); } }
+
+        public static Vect2 operator +(Vect2 a, Vect2 b)
+        { return new Vect2(a.x + b.x, a.y + b.y); }
+        public static Vect2 operator -(Vect2 a, Vect2 b)
+        { return new Vect2(a.x - b.x, a.y - b.y); }
+        public static Vect2 operator *(Vect2 a, double b)
+        { return new Vect2(a.x * b, a.y * b); }
+        public static Vect2 operator /(Vect2 a, double b)
+        { return new Vect2(a.x / b, a.y / b); }
+        public static Vect2 operator *(double a, Vect2 b)
+        { return new Vect2(a * b.x, a * b.y); }
+        public static Vect2 operator /(double a, Vect2 b)
+        { return new Vect2(a / b.x, a / b.y); }
+    }
+
+    public class Vect3
+    {
+        public double x;
+        public double y;
+        public double u; // Horizontal extra-dimension: blue to red
+
+        /// <summary>
+        /// Magnitude of a vector: its length calculated by the Pythagorean Theorem
+        /// </summary>
+        public double M { get { return Math.Sqrt(x * x + y * y + u * u); } }
+
+        public Vect3(double x, double y, double u)
+        {
+            this.x = x;
+            this.y = y;
+            this.u = u;
+        }
+
+        public static Vect3 Zero
+        { get { return new Vect3(0, 0, 0); } }
+        public static Vect3 Unit
+        { get { return new Vect3(1, 1, 1); } }
+
+        public static Vect3 operator +(Vect3 a, Vect3 b)
+        { return new Vect3(a.x + b.x, a.y + b.y, a.u + b.u); }
+        public static Vect3 operator -(Vect3 a, Vect3 b)
+        { return new Vect3(a.x - b.x, a.y - b.y, a.u - b.u); }
+
+        public static Vect3 operator +(Vect3 a, Vect2 b)
+        { return new Vect3(a.x + b.x, a.y + b.y, a.u); }
+        public static Vect3 operator +(Vect2 a, Vect3 b)
+        { return new Vect3(a.x + b.x, a.y + b.y, b.u); }
+        public static Vect3 operator -(Vect3 a, Vect2 b)
+        { return new Vect3(a.x - b.x, a.y - b.y, a.u); }
+        public static Vect3 operator -(Vect2 a, Vect3 b)
+        { return new Vect3(a.x - b.x, a.y - b.y, b.u); }
+
+        public static Vect3 operator *(Vect3 a, double b)
+        { return new Vect3(a.x * b, a.y * b, a.u * b); }
+        public static Vect3 operator /(Vect3 a, double b)
+        { return new Vect3(a.x / b, a.y / b, a.u / b); }
+        public static Vect3 operator *(double a, Vect3 b)
+        { return new Vect3(a * b.x, a * b.y, a * b.u); }
+        public static Vect3 operator /(double a, Vect3 b)
+        { return new Vect3(a / b.x, a / b.y, a / b.u); }
+    }
+
+    public class Vect4
+    {
+        public double x;
+        public double y;
+        public double u; // Horizontal extra-dimension: blue to red
+        public double v; // Vertical extra-dimension: green to yellow
+
+        /// <summary>
+        /// Magnitude of a vector: its length calculated by the Pythagorean Theorem
+        /// </summary>
+        public double M { get { return Math.Sqrt(x * x + y * y + u * u + v * v); } }
+
+        public Vect4(double x, double y, double u, double v)
+        {
+            this.x = x;
+            this.y = y;
+            this.u = u;
+            this.v = v;
+        }
+
+        public static Vect4 Zero
+        { get { return new Vect4(0, 0, 0, 0); } }
+        public static Vect4 Unit
+        { get { return new Vect4(1, 1, 1, 1); } }
+
+        public static Vect4 operator +(Vect4 a, Vect4 b)
+        { return new Vect4(a.x + b.x, a.y + b.y, a.u + b.u, a.v + b.v); }
+        public static Vect4 operator -(Vect4 a, Vect4 b)
+        { return new Vect4(a.x - b.x, a.y - b.y, a.u - b.u, a.v - b.v); }
+
+        public static Vect4 operator +(Vect4 a, Vect3 b)
+        { return new Vect4(a.x + b.x, a.y + b.y, a.u + b.u, a.v); }
+        public static Vect4 operator +(Vect3 a, Vect4 b)
+        { return new Vect4(a.x + b.x, a.y + b.y, a.u + b.u, b.v); }
+        public static Vect4 operator -(Vect4 a, Vect3 b)
+        { return new Vect4(a.x - b.x, a.y - b.y, a.u - b.u, a.v); }
+        public static Vect4 operator -(Vect3 a, Vect4 b)
+        { return new Vect4(a.x - b.x, a.y - b.y, a.u - b.u, b.v); }
+
+        public static Vect4 operator +(Vect4 a, Vect2 b)
+        { return new Vect4(a.x + b.x, a.y + b.y, a.u, a.v); }
+        public static Vect4 operator +(Vect2 a, Vect4 b)
+        { return new Vect4(a.x + b.x, a.y + b.y, b.u, b.v); }
+        public static Vect4 operator -(Vect4 a, Vect2 b)
+        { return new Vect4(a.x - b.x, a.y - b.y, a.u, a.v); }
+        public static Vect4 operator -(Vect2 a, Vect4 b)
+        { return new Vect4(a.x - b.x, a.y - b.y, b.u, b.v); }
+
+        public static Vect4 operator *(Vect4 a, double b)
+        { return new Vect4(a.x * b, a.y * b, a.u * b, a.v * b); }
+        public static Vect4 operator /(Vect4 a, double b)
+        { return new Vect4(a.x / b, a.y / b, a.u / b, a.v / b); }
+        public static Vect4 operator *(double a, Vect4 b)
+        { return new Vect4(a * b.x, a * b.y, a * b.u, a * b.v); }
+        public static Vect4 operator /(double a, Vect4 b)
+        { return new Vect4(a / b.x, a / b.y, a / b.u, a / b.v); }
+    }
+}
